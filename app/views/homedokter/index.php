90fca3845174f62e69cc7fb6213558e878b58586
yoshi_virgiawan <achmedyoshivirgiawan@gmail.com>
2020-10-29 19:04:34 +0700

    Update file yoshi

--- app/views/homedokter/index.php
+++ app/views/homedokter/index.php
@@ -243,9 +243,9 @@
 
                             <!-- Nav Item - User Information -->
                             <li class="nav-item nav-link dropdown no-arrow mr-70">
-                                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     <img class="img-profile rounded-circle" src="img/3.png">
-                                    <span class="ml-2 d-none d-lg-inline text-profile">Alif Rahman</span>
+                                    <span class="ml-2 text-profile">Alif Rahman</span>
                                 </a>
                                 <!-- Dropdown - User Information -->
                                 <div class="dropdown-menu dropdown-menu-right top-115 border-0 p-0 m-0 shadow animated--grow-in" aria-labelledby="userDropdown">
