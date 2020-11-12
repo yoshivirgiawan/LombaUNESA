e8dd11f721931dbdf305b62f17d4dc2f6078b4cb
qsaifudin <duafinis@gmail.com>
2020-11-12 22:02:59 +0700

    uder

--- templatesdokter/footer.php
+++
@@ -1,7 +0,0 @@
-</body>
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="<?= BASEURL?>/js/bootstrap.js"></script>
-    <script src="<?= BASEURL?>/js/jquery.slimscroll.min.js"></script>
-
-    <script src="<?= BASEURL?>/js/scriptdokter.js"></script>
-</html>
